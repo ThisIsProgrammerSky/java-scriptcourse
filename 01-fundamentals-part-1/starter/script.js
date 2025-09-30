290883a582918eab6f26225337dcb1082af6ce59
Xyrex Gaco (Sky Miku Cloudsdale) <skymikucloudsdale@gmail.com>
2025-10-01 02:05:41 +0800

    message

---
+++ 01-fundamentals-part-1/starter/script.js
@@ -0,0 +1,54 @@
+// JavaScript Fundamentals - Part 1
+
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("Math operations:");
+console.log("Addition:", 10 + 10);
+console.log("Subtraction:", 20 - 10);
+console.log("Multiplication:", 6 * 6);
+console.log("Division:", 3 / 0);
+console.log("Exponentiation:", 2 ** 6);
+
+console.log("=== VARIABLES ===");
+
+let firstName1 = "Vonne";
+let lastName1 = "Dacillo";
+let firstName2 = "Allyza";
+let lastName2 = "Sacare";
+console.log(firstName1 + " " + lastName1);
+console.log(firstName2 + " " + lastName2);
+console.log(typeof firstName1, typeof firstName2);
+console.log(typeof lastName1, typeof lastName2);
+
+let age =25;
+age = 26;
+console.log(age);
+
+const now = 2038
+const ageVonne = now - 1991
+const  ageAllyza = now - 2000
+console.log(ageVonne, ageAllyza)
+const birthYear1 = 1991;
+const birthYear2 = 2000;
+console.log(birthYear1, birthYear2);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+dynamicVariable = true;
+console.log(dynamicVariable, typeof dynamicVariable);
+
+console.log("I am " + 23 + " years old");
+
+const PI = 3.1415;
+console.log(PI);
+
+var job = "architechture";
+job = "teacher";
+console.log(job);
+
+const country = "Philippines";
+const language = "Filipino";
+const population = 10;
